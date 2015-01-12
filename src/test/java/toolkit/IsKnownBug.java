1376f42f22e55268a1b359f20465dc3feb25da3e
Sergey.Kashapov <skashapov@imhoclub.ru>
2015-01-12 11:24:36 +0300

    add annotation isKnownBug

---
+++ src/test/java/toolkit/IsKnownBug.java
@@ -0,0 +1,17 @@
+package toolkit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Restore on 1/12/15.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IsKnownBug {
+    public String getBugUrl();
+
+    public String getBugDescription() default "";
+}
--- src/test/java/toolkit/driver/WebDriverListener.java
+++ src/test/java/toolkit/driver/WebDriverListener.java
@@ -2,9 +2,11 @@ package toolkit.driver;
 
 import com.google.common.base.Throwables;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
+import toolkit.IsKnownBug;
 import toolkit.helpers.OperationsHelper;
 
 import java.util.concurrent.ConcurrentSkipListSet;
@@ -27,6 +29,10 @@ public class WebDriverListener implements IInvokedMethodListener {
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
         if (invocateds.add(System.identityHashCode(testResult))) {
+            if (method.getTestMethod().getMethod().getAnnotation(IsKnownBug.class) != null && testResult.getStatus() == ITestResult.FAILURE) {
+                IsKnownBug clazz = testResult.getMethod().getMethod().getAnnotation(IsKnownBug.class);
+                Assert.fail(clazz.getBugUrl() + " " + clazz.getBugDescription());
+            }
             if (!testResult.isSuccess() && method.isTestMethod()) {
                 OperationsHelper.makeScreenshot(testResult.getName());
                 log4j.error(
