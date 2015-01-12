bbdef3ced3d08acf7ebce16351d6385752f72539
Sergey.Kashapov <skashapov@imhoclub.ru>
2015-01-12 11:27:09 +0300

    improve http client

--- src/test/java/toolkit/helpers/RequestClient.java
+++ src/test/java/toolkit/helpers/RequestClient.java
@@ -1,12 +1,15 @@
 package toolkit.helpers;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.*;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
@@ -20,6 +23,14 @@ public class RequestClient {
     private String baseUrl = YamlConfigProvider.getStageParameters("baseUrl");
     static Logger log = Logger.getLogger(RequestClient.class);
     String requestLine = "";
+    RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.BEST_MATCH).build();
+    CookieStore cookieStore = new BasicCookieStore();
+    HttpClientContext context = HttpClientContext.create();
+    CloseableHttpClient httpClient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).setDefaultRequestConfig(globalConfig).setDefaultCookieStore(cookieStore).build();
+
+    {
+        context.setCookieStore(cookieStore);
+    }
 
 
     public RequestClient sendRequest(METHODS method, String url, List<BasicNameValuePair> params) {
@@ -31,7 +42,7 @@ public class RequestClient {
             HttpRequestBase request = method.getMethod(builder.build());
             requestLine = request.getRequestLine().toString();
             log.info("Request is " + requestLine);
-            HttpResponse response = HttpClientBuilder.create().build().execute(request);
+            HttpResponse response = httpClient.execute(request, context);
             responseText = EntityUtils.toString(response.getEntity(), "UTF-8");
             log.info("Response is " + responseText);
         } catch (Exception e) {
@@ -49,7 +60,7 @@ public class RequestClient {
             HttpPost request = new HttpPost(builder.build());
             request.setEntity(new UrlEncodedFormEntity(params));
             log.info("Request is " + request.getRequestLine());
-            HttpResponse response = HttpClientBuilder.create().build().execute(request);
+            HttpResponse response = httpClient.execute(request,context);
             responseText = new BasicResponseHandler().handleResponse(response);
             log.info("Response is " + responseText);
         } catch (Exception e) {
