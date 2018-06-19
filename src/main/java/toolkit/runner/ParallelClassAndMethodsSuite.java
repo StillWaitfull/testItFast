718a3decc53fac79997a63b18d130f50346025e2
Sergey Kashapov <s.kashapov@corp.mail.ru>
2018-06-19 15:28:17 +0300

    fix

--- src/main/java/toolkit/runner/ParallelClassAndMethodsSuite.java
+++ src/main/java/toolkit/runner/ParallelClassAndMethodsSuite.java
@@ -1,15 +1,10 @@
 package toolkit.runner;
 
-import org.junit.runner.Runner;
-import org.junit.runners.ParentRunner;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.junit.runners.model.RunnerScheduler;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,9 +12,9 @@ import java.util.concurrent.TimeUnit;
 public class ParallelClassAndMethodsSuite extends Suite {
 
     private static final int numberOfThreads = (System.getenv("numberOfThreads") == null ? 8 : Integer.parseInt(System.getenv("numberOfThreads")));
-    private static final ExecutorService fService = Executors.newFixedThreadPool(numberOfThreads);
+    private final ExecutorService fService = Executors.newFixedThreadPool(numberOfThreads);
 
-    private static final RunnerScheduler schedulerForClasses = new RunnerScheduler() {
+    private final RunnerScheduler schedulerForClasses = new RunnerScheduler() {
         public void schedule(Runnable childStatement) {
             fService.submit(childStatement);
         }
@@ -38,25 +33,5 @@ public class ParallelClassAndMethodsSuite extends Suite {
     public ParallelClassAndMethodsSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError {
         super(klass, builder);
         setScheduler(schedulerForClasses);
-        getChildren().forEach(ParallelClassAndMethodsSuite::parallelize);
-    }
-
-    private static Runner parallelize(Runner runner) {
-        if (runner instanceof ParentRunner) {
-            ((ParentRunner<?>) runner).setScheduler(new RunnerScheduler() {
-                private List<CompletableFuture> completableFutures = new ArrayList<>();
-
-                @Override
-                public void schedule(Runnable childStatement) {
-                    completableFutures.add(CompletableFuture.runAsync(childStatement));
-                }
-
-                @Override
-                public void finished() {
-                    CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size()])).join();
-                }
-            });
-        }
-        return runner;
     }
 }
