4abd7d1256d13b95294073051f9c48e3e081cca9
Sergey Kashapov <s.kashapov@corp.mail.ru>
2018-06-20 08:40:39 +0300

    refactoring

--- src/main/java/toolkit/runner/ParallelClassAndMethodsSuite.java
+++ src/main/java/toolkit/runner/ParallelClassAndMethodsSuite.java
@@ -1,37 +1,66 @@
 package toolkit.runner;
 
+import configs.PlatformConfig;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runners.ParentRunner;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.junit.runners.model.RunnerScheduler;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class ParallelClassAndMethodsSuite extends Suite {
 
-    private static final int numberOfThreads = (System.getenv("numberOfThreads") == null ? 8 : Integer.parseInt(System.getenv("numberOfThreads")));
-    private final ExecutorService fService = Executors.newFixedThreadPool(numberOfThreads);
+    private static final int numberOfThreads = (System.getenv("numberOfThreads") == null ? 10 : Integer.parseInt(System.getenv("numberOfThreads")));
+    public static final int PARALLEL_METHODS = 3;
+    private final ExecutorService fService = Executors.newFixedThreadPool(numberOfThreads / PARALLEL_METHODS);
 
-    private final RunnerScheduler schedulerForClasses = new RunnerScheduler() {
-        public void schedule(Runnable childStatement) {
-            fService.submit(childStatement);
-        }
 
-        public void finished() {
-            try {
-                fService.shutdown();
-                fService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace(System.err);
+    private RunnerScheduler getSchedulerForClasses(ExecutorService executorService) {
+        return new RunnerScheduler() {
+            public void schedule(Runnable childStatement) {
+                executorService.submit(childStatement);
             }
-        }
-    };
 
+            public void finished() {
+                try {
+                    executorService.shutdown();
+                    executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        };
+    }
 
     public ParallelClassAndMethodsSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError {
         super(klass, builder);
-        setScheduler(schedulerForClasses);
+        setScheduler(getSchedulerForClasses(fService));
+        List<Runner> children = getChildren();
+        children.forEach(this::parallelize);
+    }
+
+    private Runner parallelize(Runner runner) {
+        if (runner instanceof ParentRunner) {
+            ((ParentRunner<?>) runner).setScheduler(getSchedulerForClasses(Executors.newFixedThreadPool(PARALLEL_METHODS)));
+        }
+        return runner;
+    }
+
+    @Override
+    public Description getDescription() {
+        try {
+            Class currentClass = getTestClass().getJavaClass();
+            PlatformConfig platformConfig = (PlatformConfig) currentClass.getField("PLATFORM_CONFIG").get(currentClass);
+            PlatformConfig.setConfigToThread(platformConfig);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return super.getDescription();
     }
 }
--- src/test/java/tests/suites/GoogleSuite.java
+++ src/test/java/tests/suites/GoogleSuite.java
@@ -1,28 +1,28 @@
 package tests.suites;
 
-import com.googlecode.junittoolbox.ParallelSuite;
 import configs.PlatformConfig;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.openqa.selenium.Dimension;
 import tests.simpleTests.GoogleTest;
+import toolkit.runner.ParallelClassAndMethodsSuite;
 
 import static common.Platform.PC;
 
 
-@RunWith(ParallelSuite.class)
+@RunWith(ParallelClassAndMethodsSuite.class)
 @Suite.SuiteClasses({GoogleTest.class})
 public class GoogleSuite {
 
-    private final static PlatformConfig CHROME_FULL_SCREEN = new PlatformConfig(
+    private final static PlatformConfig PLATFORM_CONFIG = new PlatformConfig(
             "chrome",
             new Dimension(1920,800),
             PC);
 
     @BeforeClass
     public static void setUp() {
-        PlatformConfig.setConfigToThread(CHROME_FULL_SCREEN);
+        PlatformConfig.setConfigToThread(PLATFORM_CONFIG);
     }
 
 }
